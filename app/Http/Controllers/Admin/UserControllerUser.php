5e9bde6bd6d8fd4940fa42b9c788504254868869
EzraLouis22 <louisfra22@gmail.com>
2025-06-25 21:39:50 +0700

    update

--- app/Http/Controllers/Admin/UserControllerUser.php
+++ app/Http/Controllers/Admin/UserControllerUser.php
@@ -58,7 +58,7 @@ class UserControllerUser extends Controller
         $user->email = $validated['email'];
         $user->password = bcrypt($validated['password']);
         $user->role = 'member';
-        $user->image = null;
+        $user->image = filename($request->file('image'));
 
         if ($request->hasFile('image')) {
             $file = $request->file('image');
