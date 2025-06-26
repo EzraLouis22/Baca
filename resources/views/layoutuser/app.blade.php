70a13aaff86920f3e18a498ca9e855b81114373c
EzraLouis22 <louisfra22@gmail.com>
2025-06-27 01:48:58 +0700

    update

--- resources/views/layoutuser/app.blade.php
+++ resources/views/layoutuser/app.blade.php
@@ -64,58 +64,87 @@
         width: 80%;
         transition: all 0.3s ease-in-out;
       }
-      .logo-menu ul {
-        margin: 10px auto; /* Kurangi margin untuk membuatnya lebih sesuai dengan layar handphone */
-        padding: 5px;
+      .logo-menu div {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        margin: 0 auto;
         transition: all 0.3s ease-in-out;
-        font-size: 20px;
+        text-align: center;
+        padding: 15px;
+        grid-gap: 10px;
       }
-      .logo-menu a {
+
+      .logo-menu div button {
         color: #fff;
         text-decoration: none;
         transition: all 0.3s ease-in-out;
+        background-color: #333;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        font-size: 14px;
+        cursor: pointer;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
       }
-      .logo-menu a:hover {
+
+      .logo-menu div button:hover {
         color: #ccc;
+        background-color: #555;
         transition: all 0.3s ease-in-out;
       }
     }
+    
     @media only screen and (min-width: 769px) {
-      /* Gaya untuk layar Laptop dan desktop */
+      /* Gaya untuk layar laptop */
       nav img {
         width: 100px; /* Ubah ukuran gambar navigasi menjadi lebih besar */
       }
 
       .logo-menu {
         display: flex;
-        flex-direction: row;
+        justify-content: flex-center;
         align-items: center;
-        margin: 0 auto;
-        width: 100%;
-        text-align: center;
+        margin-left: 0;
+        width: 66%;
         transition: all 0.3s ease-in-out;
-        justify-content: center; /* Tambahkan properti ini untuk membuat navigasi lebih ke tengah */
       }
+
       .logo-menu img {
-        margin-left: 0; /* Pindahkan logo ke paling kiri */
-        width: 10%;
+        display: block;
+        margin-left: 0;
+        width: 25%;
         transition: all 0.3s ease-in-out;
       }
-      .logo-menu ul {
-        margin: 0 auto;
-        transition: all 0.3s ease-in-out;
-        font-size: 1.5rem;
-        flex-grow: 1;
-        text-align: center;
-        margin-right: 200px; /* Tambahkan properti ini untuk membuat navigasi lebih ke tengah */
+
+      .logo-menu div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 15px;
+        grid-gap: 10px;
       }
-      .logo-menu a {
+      
+      .logo-menu div button {
+        margin-left: 10px;
         color: #fff;
         text-decoration: none;
         transition: all 0.3s ease-in-out;
+        background-color: #333;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        font-size: 14px;
+        cursor: pointer;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
       }
-      .logo-menu a:hover {
+
+      .logo-menu div button:hover {
         color: #ccc;
+        background-color: #555;
         transition: all 0.3s ease-in-out;
       }
     }
@@ -124,43 +153,48 @@
   <header class="shadow text-white">
     <nav class="px-4 py-2 shadow">
       <div class="logo-menu">
-        <img src="{{ asset('picture/logo_baca.png') }}" alt="BACA Perkantas Logo" class="w-40 h-auto" />
-        <ul class="flex flex-wrap items-center justify-center space-x-4 mt-2 md:mt-0">
-          <li>
-              <a href="{{ route('user.auth.beranda') }}" class="hover:text-gray-900 flex items-center">
-                  <svg class="w-4 h-4 mr-2" xmlns="[http://www.w3.org/2000/svg"](http://www.w3.org/2000/svg") fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                  </svg>
-                  Beranda
-              </a>
-          </li>
+        <img src="{{ asset('picture/logo_baca.png') }}" alt="BACA Perkantas Logo"/>
+          <div class="menu">
+            <form action="{{ route('user.auth.beranda') }}" method="get">
+                <button type="submit" class="hover:text-gray-900 flex items-center">
+                    <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+                    </svg>
+                    Beranda
+                </button>
+            </form>
 
-          <li>
-              <a href="{{ route('user.renungans.index') }}" class="hover:text-gray-900 flex items-center">
-                  <svg class="w-4 h-4 mr-2" xmlns="[http://www.w3.org/2000/svg"](http://www.w3.org/2000/svg") fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.477 6.936 9.268 8.03 7.5 9.4c-4.418 3.444-1.29 8.484 5.12 8.484V16c-3.075.034-5.068-1.464-6.158-3.498A1.989 1.989 0 000 9.05v-1.833c0-3.214 1.571-6.046 4.25-6.046 1.603 0 3.125.545 4.25 1.455V2.25c0-.966.626-1.75 1.475-1.75a1.74 1.74 0 013.02 0c.85.787 1.475 2.15 1.475 3.53v6.135A1.74 1.74 0 0112 9.73v1.833c0 3.214-1.571 6.046-4.25 6.046-1.603 0-3.125-.545-4.25-1.455V14.4c0-.966-.626-1.75-1.475-1.75a1.74 1.74 0 01-3.02 0c-.85.787-1.475 2.15-1.475 3.53v1.833A1.74 1.74 0 0112 21.73v2.135z" />
-                  </svg>
-                  Renungan
-              </a>
-          </li>
+            <form action="{{ route('user.renungans.index') }}" method="get">
+                <button type="submit" class="hover:text-gray-900 flex items-center">
+                    <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M12 6.253v13m0-13C10.477 6.936 9.268 8.03 7.5 9.4c-4.418 3.444-1.29 8.484 5.12 8.484V16c-3.075.034-5.068-1.464-6.158-3.498A1.989 1.989 0 000 9.05v-1.833c0-3.214 1.571-6.046 4.25-6.046 1.603 0 3.125.545 4.25 1.455V2.25c0-.966.626-1.75 1.475-1.75a1.74 1.74 0 013.02 0c.85.787 1.475 2.15 1.475 3.53v6.135A1.74 1.74 0 0112 9.73v1.833c0 3.214-1.571 6.046-4.25 6.046-1.603 0-3.125-.545-4.25-1.455V14.4c0-.966-.626-1.75-1.475-1.75a1.74 1.74 0 01-3.02 0c-.85.787-1.475 2.15-1.475 3.53v1.833A1.74 1.74 0 0112 21.73v2.135z"/>
+                    </svg>
+                    Renungan
+                </button>
+            </form>
 
-          <li>
-              <a href="{{ route('user.catatan.index') }}" class="hover:text-gray-900 flex items-center">
-                  <svg class="w-4 h-4 mr-2" xmlns="[http://www.w3.org/2000/svg"](http://www.w3.org/2000/svg") fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V3a2 2 0 012-2h12a2 2 0 012 2v12a2 2 0 01-2 2h-6" />
-                  </svg>
-                  Catatanku
-              </a>
-          </li>
-          <li>
-              <a href="{{ route('user.auth.profile') }}" class="hover:text-gray-900 flex items-center">
-                  <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                  </svg>
-                  Profile
-              </a>
-          </li>
-        </ul>
+            <form action="{{ route('user.catatan.index') }}" method="get">
+                <button type="submit" class="hover:text-gray-900 flex items-center">
+                    <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V3a2 2 0 012-2h12a2 2 0 012 2v12a2 2 0 01-2 2h-6"/>
+                    </svg>
+                    Catatanku
+                </button>
+            </form>
+
+            <form action="{{ route('user.auth.profile') }}" method="get">
+                <button type="submit" class="hover:text-gray-900 flex items-center">
+                    <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
+                    </svg>
+                    Profile
+                </button>
+            </form>
+        </div>
       </div>
     </nav>
   </header>
