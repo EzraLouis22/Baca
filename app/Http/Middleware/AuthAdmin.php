58ec7491dadf784572bf8c9b51a9ce83ccc59714
EzraLouis22 <louisfra22@gmail.com>
2025-07-02 03:10:40 +0700

    update

--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -9,6 +9,7 @@ class AuthAdmin
     public function handle($request, Closure $next)
     {
         if (Auth::user()->role !== 'admin') {
+            Alert::error('Anda bukan admin', 'Error');
             return redirect()->route('login');
         }
         return $next($request);
--- app/Http/Middleware/AuthMember.php
+++ app/Http/Middleware/AuthMember.php
@@ -2,6 +2,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Alert;
 use Illuminate\Support\Facades\Auth;
 
 class AuthMember
@@ -9,6 +10,7 @@ class AuthMember
     public function handle($request, Closure $next)
     {
         if (Auth::user()->role !== 'member') {
+            Alert::error('Anda bukan member', 'Error');
             return redirect()->route('login');
         }
         return $next($request);
